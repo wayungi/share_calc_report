195e7965bdd0d55db778c67c53ace4ed27728b82
wayungi <fwayungi.com>
2023-12-07 22:37:10 +0300

    REFACTOR: regex for matching draw number

--- util/dataExtraction.js
+++ util/dataExtraction.js
@@ -25,12 +25,11 @@ const getProductName = (filePath) => {
 }
 
 const getDrawNumber = (line) => {
-  // this needs to be optimized
-  const regex = /[A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z]\s[A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z]\s[A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z][A-Za-z]\s[A-Za-z][A-Za-z][A-Za-z]\s[A-Za-z][A-Za-z][A-Za-z][A-Za-z]\s\s\s\s\s\s\s\s\s\s\d\d\d\d/
+  const regex = /Calculated share values for draw\s+\d+/
   const matchResult = regex.exec(line)
   if (!matchResult) return
   const drawNumber = matchResult[0].split(' ').pop()
-  console.log('Draw Number :', `${drawNumber}`)
+  console.log(drawNumber)
   return drawNumber
 }
 
@@ -43,15 +42,14 @@ const getRollOverNumber = (line) => {
   const matchResult = regex.exec(line)
   if (!matchResult) return
   const rollOverNumber = matchResult[0].split(' ').pop()
-  console.log('Roll Over Number', `${rollOverNumber}`)
   return rollOverNumber
 }
 
 const getGameStats = (line) => {
-  const regex = /\d+\s+\d{1,3}(,\d{3})*(\.\d{2})\s+\d+\s+\d{1,3}(,\d{3})*(\.\d{2})\s+(-)*\d{1,3}(,\d{3})*(\.\d{2})/
+  const regex = /\d+\s+\d{1,3}(,\d{3})*(\.\d{2})\s+\d+\s+\d{1,3}(,\d{3})*(\.\d{2})/
   const matchResult = regex.exec(line)
   if (!matchResult) return
-  console.log(matchResult)
+  return matchResult[0]
 }
 
 module.exports = {
