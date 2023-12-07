0bc88b3af1c2a8e635d849437479dac67dc5f4c4
wayungi <fwayungi.com>
2023-12-07 23:03:39 +0300

    REFACTOR: optimize dataExtraction module

--- util/dataExtraction.js
+++ util/dataExtraction.js
@@ -5,45 +5,17 @@ const ProductCode = {
 }
 
 const getProductName = (filePath) => {
-  let productName = ''
-  if (filePath.includes(ProductCode.LOTTO)) {
-    productName = 'LOTTO'
-  } else if (filePath.includes(ProductCode.DAILYLOTTO)) {
-    productName = 'DAILYLOTTO'
-  } else if (filePath.includes(ProductCode.POWERBALL)) {
-    productName = 'POWERBALL'
-  }
-  return productName
-}
-
-const getDrawNumber = (line) => {
-  const regex = /Calculated share values for draw\s+\d+/
-  const matchResult = regex.exec(line)
-  if (!matchResult) return
-  const drawNumber = matchResult[0].split(' ').pop()
-  console.log(drawNumber)
-  return drawNumber
+  if (filePath.includes(ProductCode.LOTTO)) return 'LOTTO'
+  if (filePath.includes(ProductCode.DAILYLOTTO)) return 'DAILYLOTTO'
+  if (filePath.includes(ProductCode.POWERBALL)) return 'POWERBALL'
 }
 
 const getNextDrawRollOver = (line) => {
   console.log('Still pending')
 }
-
-const getRollOverNumber = (line) => {
-  const regex = /Rollover number\s+\d+/
-  const matchResult = regex.exec(line)
-  if (!matchResult) return
-  const rollOverNumber = matchResult[0].split(' ').pop()
-  console.log(rollOverNumber)
-  return rollOverNumber
-}
-
-const getGameStats = (line) => {
-  const regex = /\d+\s+\d{1,3}(,\d{3})*(\.\d{2})\s+\d+\s+\d{1,3}(,\d{3})*(\.\d{2})/
-  const matchResult = regex.exec(line)
-  if (!matchResult) return
-  return matchResult[0]
-}
+const getDrawNumber = (match) => match[0].split(' ').pop()
+const getRollOverNumber = (match) => match[0].split(' ').pop()
+const getGameStats = (match) => match[0]
 
 module.exports = {
   getProductName,
